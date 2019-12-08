89238ffddf93f15ec608e9fbe0fc976db7e041e9
Shoaib Sayyed <sayyedshoaib57@gmail.com>
2019-12-09 00:46:55 +0530

    First Commit

---
+++ js/header.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
